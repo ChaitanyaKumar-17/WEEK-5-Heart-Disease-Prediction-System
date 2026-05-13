523cc52f16eb5f0bdcbb353e0e873f5fcc1f2222
Chaitanya Kumar <chaitanya0817@gmail.com>
2026-05-14 00:13:00 +0530

    spliting dataset into training and testing sets

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from ucimlrepo import fetch_ucirepo 
+from sklearn.model_selection import train_test_split
   
 # fetch the Heart Disease dataset
 heart_disease = fetch_ucirepo(id=45) 
@@ -7,7 +8,19 @@ print(df.head())
 print(df.info())
 
 # Splitting dataDrame as features and target
-X = heart_disease.data.features
+X = heart_disease.data.features.copy()
 print(X.head())
-y = heart_disease.data.targets
+y = heart_disease.data.targets.copy()
 print(y.head())
+
+# Preprocessing
+X['ca'] = X['ca'].fillna(X['ca'].mode()[0])
+X['thal'] = X['thal'].fillna(X['thal'].mode()[0])
+
+X['ca'] = X['ca'].astype('int64')
+X['thal'] = X['thal'].astype('int64')
+
+print(X.info())
+
+# Split into training and testing sets to prevent data leakage
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
